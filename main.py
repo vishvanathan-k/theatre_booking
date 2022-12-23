540c11c05dc372e60a8d40b44dbd530c9b689cc6
Vishvanathan K <vishvanathank@outlook.com>
2022-12-23 18:44:21 +0530

    Admins can view how many seats are booked

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import file
 import _var
 from encrypt import hash
 
-# TODO: admin able to see who has booked what seat
+# TODO: admin able to see who has booked what seat, beverages and snacks
 Questions = [
     "In which year were you born?",
     "Where were you born?",
@@ -255,6 +255,7 @@ class Admin_Page(QDialog):
         self.signout.clicked.connect(self.signoutfunc)
         self.AddMov.clicked.connect(self.addmov)
         self.RemoveMov.clicked.connect(self.removemov)
+        self.BookedSeats.clicked.connect(self.GotoSee)
         self.AdminTable.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
         self.AdminTable.setHorizontalHeaderLabels(
             [
@@ -457,6 +458,66 @@ class Admin_Page(QDialog):
         self.dialog.close()
         return None
 
+    def GotoSee(self):
+        try:
+            row = self.AdminTable.currentRow()
+            _var.Cinema = self.AdminTable.item(row, 2).text()
+            _var.Screen = self.AdminTable.item(row, 1).text()
+            _var.Timing = self.AdminTable.item(row, 3).text()
+        except AttributeError:
+            return None
+        clear(widget)
+        f = Seat_see()
+        widget.addWidget(f)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class Seat_see(QDialog):
+    def __init__(self):
+        super(Seat_see,self).__init__()
+        loadUi("seats_see.ui",self)
+        self.seats_list = []
+        self.seats = file.read_seats(_var.Screen, _var.Cinema, _var.Timing)
+        self.rows = 10
+        self.columns = 10
+        self.label_2.setText("Cinema: "+ _var.Cinema+ " Screen: "+ _var.Screen+ " Timing: "+ _var.Timing)
+
+        _list = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10",
+                 "B1","B2","B3","B4","B5","B6","B7","B8","B9","B10",
+                 "C1","C2","C3","C4","C5","C6","C7","C8","C9","C10",
+                 "D1","D2","D3","D4","D5","D6","D7","D8","D9","D10",
+                 "E1","E2","E3","E4","E5","E6","E7","E8","E9","E10",
+                 "F1","F2","F3","F4","F5","F6","F7","F8","F9","F10",
+                 "G1","G2","G3","G4","G5","G6","G7","G8","G9","G10",
+                 "H1","H2","H3","H4","H5","H6","H7","H8","H9","H10",
+                 "I1","I2","I3","I4","I5","I6","I7","I8","I9","I10",
+                 "K1","K2","K3","K4","K5","K6","K7","K8","K9","K10",
+                 "L1","L2","L3","L4","L5","L6","L7","L8","L9","L10",]
+        len_list = len(_list)
+
+        i = 0
+        for row in range(self.rows):
+            for column in range(self.columns):
+                button = PushButton(f"{_list[i]}", self)
+                if _list[i] in self.seats:
+                    button.setStyleSheet("background-color: red")
+                elif _list[i] in self.seats_list:
+                    button.setStyleSheet("background-color: orange")
+                else:
+                    button.setStyleSheet("background-color: green")
+                self.gridLayout.addWidget(button, row + 1, column)
+                i += 1
+                if i == len_list:
+                    break
+        self.back.clicked.connect(self.back_func)
+    
+    def back_func(self):
+        clear(widget)
+        f = Admin_Page()
+        widget.addWidget(f)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+
 class Booking_History(QDialog):
     # class containing the booking history page
     def __init__(self):
@@ -578,118 +639,17 @@ class Seat_Page(QDialog):
         self.columns = 10
         self.label_2.setText("Cinema: "+ _var.Cinema+ " Screen: "+ _var.Screen+ " Timing: "+ _var.Timing)
 
-        _list = [
-            "A1",
-            "A2",
-            "A3",
-            "A4",
-            "A5",
-            "A6",
-            "A7",
-            "A8",
-            "A9",
-            "A10",
-            "B1",
-            "B2",
-            "B3",
-            "B4",
-            "B5",
-            "B6",
-            "B7",
-            "B8",
-            "B9",
-            "B10",
-            "C1",
-            "C2",
-            "C3",
-            "C4",
-            "C5",
-            "C6",
-            "C7",
-            "C8",
-            "C9",
-            "C10",
-            "D1",
-            "D2",
-            "D3",
-            "D4",
-            "D5",
-            "D6",
-            "D7",
-            "D8",
-            "D9",
-            "D10",
-            "E1",
-            "E2",
-            "E3",
-            "E4",
-            "E5",
-            "E6",
-            "E7",
-            "E8",
-            "E9",
-            "E10",
-            "F1",
-            "F2",
-            "F3",
-            "F4",
-            "F5",
-            "F6",
-            "F7",
-            "F8",
-            "F9",
-            "F10",
-            "G1",
-            "G2",
-            "G3",
-            "G4",
-            "G5",
-            "G6",
-            "G7",
-            "G8",
-            "G9",
-            "G10",
-            "H1",
-            "H2",
-            "H3",
-            "H4",
-            "H5",
-            "H6",
-            "H7",
-            "H8",
-            "H9",
-            "H10",
-            "I1",
-            "I2",
-            "I3",
-            "I4",
-            "I5",
-            "I6",
-            "I7",
-            "I8",
-            "I9",
-            "I10",
-            "K1",
-            "K2",
-            "K3",
-            "K4",
-            "K5",
-            "K6",
-            "K7",
-            "K8",
-            "K9",
-            "K10",
-            "L1",
-            "L2",
-            "L3",
-            "L4",
-            "L5",
-            "L6",
-            "L7",
-            "L8",
-            "L9",
-            "L10",
-        ]
+        _list = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10",
+                 "B1","B2","B3","B4","B5","B6","B7","B8","B9","B10",
+                 "C1","C2","C3","C4","C5","C6","C7","C8","C9","C10",
+                 "D1","D2","D3","D4","D5","D6","D7","D8","D9","D10",
+                 "E1","E2","E3","E4","E5","E6","E7","E8","E9","E10",
+                 "F1","F2","F3","F4","F5","F6","F7","F8","F9","F10",
+                 "G1","G2","G3","G4","G5","G6","G7","G8","G9","G10",
+                 "H1","H2","H3","H4","H5","H6","H7","H8","H9","H10",
+                 "I1","I2","I3","I4","I5","I6","I7","I8","I9","I10",
+                 "K1","K2","K3","K4","K5","K6","K7","K8","K9","K10",
+                 "L1","L2","L3","L4","L5","L6","L7","L8","L9","L10",]
         len_list = len(_list)
 
         i = 0
